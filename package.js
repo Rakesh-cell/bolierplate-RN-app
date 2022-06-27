245c5356d487c336f0fbb2946be9aaa733739859
Rakesh-cell <55280274+Rakesh-cell@users.noreply.github.com>
2022-06-27 15:31:21 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,8 @@
+{
+  "name": "boilerplate-RN-app",
+  "version": "0.0.5",
+  "description": "React Native Template",
+  "repository": "git@github.com:Esemesek/react-native-new-template.git",
+  "author": "Rakesh <rakm030@gmail.com>",
+  "license": "MIT"
+}
