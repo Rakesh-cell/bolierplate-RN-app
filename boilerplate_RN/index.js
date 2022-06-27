f14555171d3907c217afcce6eeac1674939b1cd1
Rakesh-cell <55280274+Rakesh-cell@users.noreply.github.com>
2022-06-27 15:41:28 +0530

    Update index.js

--- boilerplate_RN/index.js
+++ boilerplate_RN/index.js
@@ -4,11 +4,11 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+// import {name as appName} from './app.json';
 // import fontsCSS from "@native-base/icons/FontsCSS";
 // const style = document.createElement("style");
 // style.type = "text/css";
 // style.appendChild(document.createTextNode(fontsCSS));
 // document.head.appendChild(style); 
-// const APP_NAME = 'ProjectName';
-AppRegistry.registerComponent(appName, () => App);
+const APP_NAME = 'ProjectName';
+AppRegistry.registerComponent(APP_NAMe, () => App);
